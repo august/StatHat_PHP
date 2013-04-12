b848a998fe9e462c720a6b96877a6df2eec4a01d
August Trometer <august@getyowza.com>
2013-04-11 23:36:21 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-StatHat_PHP
-===========
+# StatHat PHP Class #
 
-A PHP class that publishes data to StatHat
+A PHP class that publishes data to [StatHat](http://stathat.com/), based, in part, on StatHat's own `stathat.php`.
+
+The class includes support for transactions, allowing you to post multiple stats with a single `POST`. If you are publishing several stats at a time, this is more efficient and consumes fewer server resources.
+
+The StatHat class is meant to be used statically. This allows a little more flexibility than creating an actual object. If your site has multiple includes (as most do), you can use a single transaction across all the files. 
+
+For example, you could start the transaction in `header.php`, add stats across the rest of your includes, then commit the transaction in `footer.php`.
+
+## Usage ##
+Usage is easy.
+
+	// set the ez key
+	StatHat::setEZKey('xxxxxxxxxx');
+	
+	// start the transaction
+	StatHat::beginTransaction();
+	
+	// post a count stat
+	StatHat::publishCount('page views', 1);
+	
+	// post a count stat using a timestamp
+	StatHat::publishCount('page views', 1, time());
+	
+	// commit the transaction
+	// this posts both count stats to the server
+	StatHat::commitTransaction();
+	
+Transactions are optional. If you don't `StatHat::startTransaction()`, all stats are posted immediately.
+
+The StatHat class supports both synchronous and asynchronous posting of your stats. The default is to post them asynchronously. 
+
+	// post a count stat synchronously
+	$result = StatHat::publishCountSynchronous('page views',1);
+
+If you're using transactions, you can post the whole set synchronously as well.
+
+	// post transaction synchronously
+	$result = StatHat::commitTransactionSynchronous();
+
+	
---
+++ StatHat.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * A PHP class that facilitates posting of stats to StatHat.com
+ * Based, in part, on StatHat's own stathat.php.
+ * 
+ * Includes support for transactions.
+ *
+ * @author August Trometer (http://getyowza.com/contact)
+ */
+
+define('STATHAT_EZ_ENDPOINT','https://api.stathat.com/ez');
+
+class StatHat {
+
+	public static $_ez_key;
+	public static $_transactions = array();
+	public static $_use_transaction = false;
+
+	/**
+	 * Sets the EZ Key
+	 */
+	public static function setEZKey($key) {
+
+		self::$_ez_key = $key;
+	}
+
+	/**
+	 * Starts the transaction
+	 * 
+	 * This is only necessary if you want to send data
+	 * in a single POST
+	 */
+	public static function beginTransaction() {
+
+		self::$_use_transaction = true;
+	}
+
+	/**
+	 * Publishes a count stat
+	 * 
+	 * This function adds the data to an array of transactions. If we're
+	 * not using transactions, then the single item is instantly posted
+	 * to the server
+	 */
+	public static function publishCount($stat_key, $count, $timestamp = NULL, $synchronous = false) {
+
+		$transaction = array(
+			'stat' => $stat_key,
+			'count' => $count,
+			);
+
+		// include timestamp information if necessary
+		if ($timestamp)
+			$transaction['t'] = $timestamp;
+
+		// add to the transaction array
+		self::$_transactions[] = $transaction;
+
+		// if we're not using transactions, go ahead and 
+		// post the data
+		if (!self::$_use_transaction)
+			return self::commitTransaction($synchronous);
+	}
+
+	/**
+	 * Publishes a count stat synchronously and returns the response
+	 * 
+	 * This is a convenience function
+	 */
+	public static function publishCountSynchronous($stat_key, $count, $timestamp = NULL) {
+
+		return self::publishCount($stat_key, $count, $timestamp, true);
+	}
+
+	/**
+	 * Publishes a value stat
+	 * 
+	 * This function adds the data to an array of transactions. If we're
+	 * not using transactions, then the single item is instantly posted
+	 * to the server
+	 */
+	public static function publishValue($stat_key, $value, $timestamp = NULL, $synchronous = false) {
+
+		$transaction = array(
+			'stat' => $stat_key,
+			'value' => $value,
+			);
+
+		// include timestamp information if necessary
+		if ($timestamp)
+			$transaction['t'] = $timestamp;
+
+		self::$_transactions[] = $transaction;
+
+		if (!self::$_use_transaction)
+			return self::commitTransaction($synchronous);
+	}
+
+	/**
+	 * Publishes a value stat synchronously and returns the response
+	 * 
+	 * This is a convenience function
+	 */
+	public static function publishValueSynchronous($stat_key, $value, $timestamp = NULL) {
+
+		return self::publishValue($stat_key, $value, $timestamp, true);
+	}
+
+	/**
+	 * Posts the transaction data to the server
+	 */
+	public static function commitTransaction($synchronous = false) {
+
+		$data = array(
+			'ezkey' => self::$_ez_key,
+			'data' => array()
+			);
+
+		// build the transaction array
+		foreach(self::$_transactions as $transaction) {
+
+			$data['data'][] = $transaction;
+		}
+
+		// reset the transaction
+		self::$_use_transaction = false;
+		self::$_transactions = array();
+
+		$json = json_encode($data);
+
+		// post synchronous or asynchrnously, as desired
+		if ($synchronous)
+			return self::postJSONSynchronous($json);
+		else
+			self::postJSON($json);
+	}
+
+	/**
+	 * Posts the transaction synchronously
+	 * 
+	 * This is a convenience function
+	 */
+	public static function commitTransactionSynchronous() {
+
+		return self::commitTransaction(true);
+	}
+
+	/**
+	 * Posts the JSON data asynchronously to the server
+	 */
+	function postJSON($json) {
+
+		$parts = parse_url(STATHAT_EZ_ENDPOINT);
+
+        $fp = fsockopen($parts['host'], 80, $errno, $errstr, 30);
+
+        $out = "POST {$parts['path']} HTTP/1.1\r\n";
+        $out .= "Host: {$parts['host']}\r\n";
+        $out .= "Content-Type: application/json\r\n";
+        $out .= "Content-Length: " . strlen($json) . "\r\n";
+        $out .= "Connection: Close\r\n\r\n";
+        
+        $out .= $json;
+
+        fwrite($fp, $out);
+        fclose($fp);
+	}
+
+	/**
+	 * Posts the JSON data synchronously to the server
+	 */
+	function postJSONSynchronous($json) {
+
+		$url = STATHAT_EZ_ENDPOINT;
+
+		$params = array(
+			'http' => array(
+                'method' => 'POST',
+                'content' => $json,
+                'header' => array('Content-Type: application/json')
+        		)
+			);
+        
+        $context = stream_context_create($params);
+
+        $fp = @fopen($url, 'rb', false, $context);
+        
+        if (!$fp)
+        	throw new Exception("Problem with $url, $php_errormsg");
+        
+        $response = @stream_get_contents($fp);
+
+        if ($response === false)
+        	throw new Exception("Problem reading data from $url, $php_errormsg");
+        
+        return $response;
+	}
+
+} // clas StatHat
